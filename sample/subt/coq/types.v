cd25d6af01131af9dafc27ab3913b5fc2304017a
Elias Castegren <elias.castegren@gmail.com>
2023-12-15 11:43:18 +0100

    Added some automation and additional cases for subtyping

--- sample/subt/coq/types.v
+++ sample/subt/coq/types.v
@@ -5,6 +5,9 @@ From TLC Require Import LibSet LibTactics.
 (* Module Verona. *)
 Declare Scope verona_type_scope.
 
+Ltac auto_tilde ::= try solve [auto with verona | intuition auto].
+Ltac auto_star ::= try solve [eauto with verona | intuition eauto].
+
 (*
 t ::= t | t
     | t & t
@@ -32,7 +35,7 @@ Inductive type : Type :=
 | TConj: type -> type -> type
 | TClass: class_name -> list type -> type
 | TAlias: alias_name -> list type -> type
-| TTrait: 
+| TTrait:
     trait_name ->
     method_name ->
     list var_name -> (*method type start*)
@@ -48,29 +51,61 @@ Inductive type : Type :=
 .
 
 (* TODO options for notation level for && and || ? *)
-Notation " t1 && t2 " := (TConj t1 t2).
-Notation " t1 || t2 " := (TDisj t1 t2).
-Notation " c [ ts ] " := (TClass c ts).
-Notation " a [ ts ] " := (TAlias a ts).
-Notation " τ <{ f : [ Xs ] ts : t1 'where' t2 }> " :=
+Notation "t1 && t2" := (TConj t1 t2).
+Notation "t1 || t2" := (TDisj t1 t2).
+Notation "c [ ts ]" := (TClass c ts).
+Notation "a [ ts ]" := (TAlias a ts).
+Notation "τ <{ f : [ Xs ] ts : t1 'where' t2 }>" :=
     (TTrait τ f Xs ts t1 t2) (at level 80).
 
-Notation " 'Self' " := (TSelf).
-Notation " 'Top' " := (TTop).
-Notation " 'Bot' " := (TBot).
-Notation " t1 <: t2 " := (TSub t1 t2) (at level 50).
+Notation "'Self'" := (TSelf).
+Notation "'Top'" := (TTop).
+Notation "'Bot'" := (TBot).
+Notation "t1 <: t2" := (TSub t1 t2) (at level 50).
 
 (*
-Γ, t1, t2 ⊢ Δ
----- [conj-left]
-Γ, t1 & t2 ⊢ Δ
+alias_lookup(α, t...) = A[X...]
+Γ, A[t.../X...] ⊢ Δ
+---- [alias-left]
+Γ, α[t...] ⊢ Δ
+
+
+alias_lookup(α) = A[X...]
+Γ ⊢ Δ, A[t.../X...]
+---- [alias-right]
+Γ ⊢ Δ, α[t...]
+
+
+class_lookup(c) = A, X... // A has holes with X...
+Γ, Self <: c[t...], c[t...] <: Self, A[t.../X...] ⊢ Δ
+---- [cls-left]
+Γ, c[t...] ⊢ Δ
+// what are the ramifications of having one single Self?
+
+
+// type JSON example needs assumption that c[t...] <: c[t'...]
+∀i. Γ, c[t...] <: c[t'...] ⊢ Δ, (tᵢ <: t'ᵢ) & (t'ᵢ <: tᵢ)
+---- [cls-right]
+Γ, c[t...] ⊢ Δ, c[t'...]
+
+
+// up to renaming of parameters X...
+Γ, τ{ ... } <: σ{ ... } ⊢ s'' <: t''
+∀j.   Γ, τ{ ... } <: σ{ ... }, s'' ⊢ sⱼ <: tⱼ
+Γ, τ{ ... } <: σ{ ... }, s'' ⊢ t' <: s'
+---- [method]
+Γ, τ{ f : [X...] t... -> t' where t''} ⊢ Δ, σ{ f : [X...] s... -> s' where s'' }
+// TODO write example to illustrate this rule
 *)
 
 Open Scope verona_type_scope.
 Definition sequent := set type.
-Notation " Γ , t1 " := (Γ \u \{ t1 }) (at level 90, left associativity).
 
-Reserved Notation " Γ ⊢ Δ " (at level 95).
+Notation "Γ , t1" := (Γ \u \{ t1 }) (at level 90, left associativity).
+
+Definition type2sequent (t: type) : sequent := \{ t }.
+Coercion type2sequent : type >-> sequent.
+Reserved Notation "Γ ⊢ Δ" (at level 95).
 Inductive seq_sub : sequent -> sequent -> Prop :=
 | SubConjLeft: forall (Γ Δ:sequent) (t1 t2:type),
     Γ, t1, t2 ⊢ Δ ->
@@ -88,24 +123,119 @@ Inductive seq_sub : sequent -> sequent -> Prop :=
     Γ ⊢ Δ, t1 || t2
 | SubSyntactic: forall Γ Δ t,
     Γ, t ⊢ Δ, t
-where " Γ ⊢ Δ " := (seq_sub Γ Δ).
+| SubSubLeft: forall Γ Δ A B,
+    Γ ⊢ Δ, A ->
+    Γ, B ⊢ Δ ->
+    Γ, A <: B ⊢ Δ
+| SubSubRight: forall Γ Δ A B,
+    (* TODO: Define Γ* *)
+    (* TODO: Can has Δ* as well? *)
+    Γ(**), A ⊢ (B : type) ->
+    Γ ⊢ Δ, A <: B
+| SubTop: forall Γ Δ,
+    Γ ⊢ Δ, Top
+| SubBottom: forall Γ Δ,
+    Γ, Bot ⊢ Δ
+where "Γ ⊢ Δ" := (seq_sub Γ Δ).
 
-Definition type2sequent (t: type) : sequent := empty, t.
-Coercion type2sequent : type >-> sequent.
+Local Hint Constructors seq_sub : verona.
 
-Example ex_conj_elimination1 : forall a b,
-    a && b ⊢ a.
+Lemma sub_exact_in : forall a Γ Δ,
+    a \in (Γ: set type) ->
+    a \in (Δ: set type) ->
+    Γ ⊢ Δ.
 Proof.
-    intros a b.
-    constructor.
-    replace (\{}, a, b) with (\{}, b, a).
-    - constructor.
-    - set_prove.
+  introv Hinl Hinr.
+  apply eq_union_single_remove_one in Hinl.
+  apply eq_union_single_remove_one in Hinr.
+  rewrite union_comm in Hinl, Hinr.
+  rewrite Hinl. rewrite* Hinr.
 Qed.
 
-Example ex_conj_elimination2 : forall a b,
-    a && b ⊢ b.
+Lemma sub_top_in_right : forall Γ Δ,
+    Top \in (Δ: set type) ->
+    Γ ⊢ Δ.
 Proof.
-    intros a b.
-    constructor. constructor.
+  introv Hin.
+  apply eq_union_single_remove_one in Hin.
+  rewrite union_comm in Hin.
+  rewrite* Hin.
+Qed.
+
+Lemma sub_bot_in_left : forall Γ Δ,
+    Bot \in (Γ: set type) ->
+    Γ ⊢ Δ.
+Proof.
+  introv Hin.
+  apply eq_union_single_remove_one in Hin.
+  rewrite union_comm in Hin.
+  rewrite* Hin.
+Qed.
+
+Ltac solve_sequent :=
+  match goal with
+  | _ : _ |- _ ⊢ (type2sequent ?A) => solve [apply (sub_exact_in A); set_prove]
+  | |- _ ⊢ (type2sequent ?A) => solve [apply (sub_exact_in A); set_prove]
+  | _ : _ |- _ ⊢ _ => solve [eapply sub_exact_in; set_prove
+                            |eapply sub_top_in_right; set_prove
+                            |eapply sub_bot_in_left; set_prove]
+  |       |- _ ⊢ _ => solve [eapply sub_exact_in; set_prove
+                            |eapply sub_top_in_right; set_prove
+                            |eapply sub_bot_in_left; set_prove]
+  end.
+
+Local Hint Extern 2 => solve_sequent : verona.
+
+Example ex_conj_elimination1 : forall Γ Δ a b,
+    Γ, a && b ⊢ Δ, a.
+Proof.
+  auto with verona.
+Qed.
+
+Example ex_conj_elimination2 : forall Γ Δ a b,
+    Γ, a && b ⊢ Δ, b.
+Proof.
+    auto with verona.
+Qed.
+
+Example ex_conj_introduction : forall Γ Δ a b,
+    Γ, a, b ⊢ Δ, a && b.
+Proof.
+  auto with verona.
+Qed.
+
+Example ex_disj_introduction1 : forall Γ Δ a b,
+    Γ, a ⊢ Δ, a || b.
+Proof.
+  auto with verona.
+Qed.
+
+Example ex_disj_introduction2 : forall Γ Δ a b,
+    Γ, b ⊢ Δ, a || b.
+Proof.
+  auto with verona.
+Qed.
+
+Example ex_disj_elimination : forall Γ Δ a b,
+    Γ, a || b ⊢ Δ, a, b.
+Proof.
+  auto with verona.
+Qed.
+
+Example ex_deep_ex_falso : forall Γ Δ a b c d e,
+    Γ, Bot, a, b, c, d, e ⊢ Δ.
+Proof.
+  auto with verona.
+Qed.
+
+Example ex_deep_tauto : forall Γ Δ a b c d e,
+    Γ ⊢ Δ, Top, a, b, c, d, e.
+Proof.
+  auto with verona.
+Qed.
+
+Example ex_sub_trans : forall Γ Δ a b c,
+    Γ, a, a <: b, b <: c ⊢ Δ, a <: c.
+Proof.
+  auto with verona.
 Qed.
